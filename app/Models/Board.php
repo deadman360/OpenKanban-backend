1655ffcab1194e0d5811933365635581f35126b0
deadman360 <vmartinho.kfg@gmail.com>
2025-11-29 13:01:02 -0300

    board member pivot created

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Board extends Model
 {
@@ -23,5 +24,13 @@ class Board extends Model
     public function createdBy() : BelongsTo
     {
         return $this->belongsTo(User::class, 'created_by_id');
+    }
+
+    public function users() : BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->using(BoardMember::class)
+            ->withPivot(['role'])
+            ->withTimestamps();
     }
 }
