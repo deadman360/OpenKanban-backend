d474c88fb62c6940983376445c712feebbffefe1
deadman360 <vmartinho.kfg@gmail.com>
2025-11-29 13:14:51 -0300

    Task label pivot table created

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Label extends Model
 {
@@ -14,4 +15,11 @@ class Label extends Model
         'name',
         'color'
     ];
+
+    public function tasks() : BelongsToMany
+    {
+        return $this->belongsToMany(Task::class)
+            ->using(TaskLabel::class)
+            ->withTimestamps();
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
@@ -44,5 +45,12 @@ class Task extends Model
     public function board() : BelongsTo
     {
         return $this->belongsTo(Board::class, 'board_id');
+    }
+
+    public function labels() : BelongsToMany
+    {
+        return $this->belongsToMany(Label::class)
+            ->using(TaskLabel::class)
+            ->withTimestamps();
     }
 }
